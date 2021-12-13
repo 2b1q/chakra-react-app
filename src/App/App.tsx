1833ea03e96399d2411a9b709b66c667f70c94c8
Ilya Shevtsov <2bb3qq@gmail.com>
2021-12-13 11:21:46 +0300

    init

---
+++ src/App/App.tsx
@@ -0,0 +1,24 @@
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  VStack,
+  Grid,
+  theme,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Logo } from "../Logo";
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>Hello World!</Text>
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+);
